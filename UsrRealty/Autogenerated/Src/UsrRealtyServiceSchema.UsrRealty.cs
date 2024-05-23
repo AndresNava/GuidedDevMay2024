b6057a1d16254c8fe34bf70bfe21ab4e248d61cd
Andres Navarrete Salaz <anavarrete@hco.com>
2024-05-23 11:53:23 -0600

    Example Web Service

---
+++ UsrRealty/Autogenerated/Src/UsrRealtyServiceSchema.UsrRealty.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrRealtyServiceSchema
+
+	/// <exclude/>
+	public class UsrRealtyServiceSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrRealtyServiceSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrRealtyServiceSchema(UsrRealtyServiceSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("d10a211f-c978-4d9e-ab18-3e5e0eed6da7");
+			Name = "UsrRealtyService";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("5644c772-7dae-444c-96a5-14ec7542f86d");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,83,168,204,47,45,82,72,206,79,73,5,0,249,63,54,150,10,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("d10a211f-c978-4d9e-ab18-3e5e0eed6da7"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
